ccf993e60b852be705d850b7ef07bc27ace8be32
John Sturdy <user.email>
2020-05-13 16:23:16 +0100

    Add program for making connection charts from yaml

---
+++ make_connections_chart.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import colour
+import yaml
+
+def write_connections_chart(data, outfile, grouped, coloured):
+    outfile.write("graph connections {\n")
+    for groupname, groupmembers in data['groups'].items():
+        print(groupname, "includes", groupmembers)
+        if grouped:
+            outfile.write("    subgraph " + groupname + "_cm {\n")
+        for person in groupmembers:
+            outfile.write("        " + person + " [label=\"" + person.replace('_', ' ') + "\"")
+            if coloured:
+                outfile.write(", color=" + colour.Color(pick_for=groupname) + ", style=filled")
+            outfile.write("]\n")
+        if grouped:
+            outfile.write("    }\n")
+    for a, bs in data['connections'].items():
+        print(a, "knows", bs)
+        for b in bs.split():
+            outfile.write("    " + a + " -- " + b + "\n")
+    outfile.write("}\n")
+
+def main():
+    """Show connections between people."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-g", "--grouped", action='store_true')
+    parser.add_argument("-c", "--coloured", action='store_true')
+    parser.add_argument("inputfile")
+    args = parser.parse_args()
+    with open(args.inputfile) as infile:
+        with open(args.output or (args.inputfile + ".gv"), 'w') as outfile:
+            write_connections_chart(yaml.load(infile,
+                                              Loader=yaml.SafeLoader),
+                                    outfile,
+                                    args.grouped,
+                                    args.coloured)
+
+if __name__ == "__main__":
+    main()
