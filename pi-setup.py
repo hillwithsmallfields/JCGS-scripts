25b1384be54b560f0ca31e27d54a6ffd375d1c78
John Sturdy <jcg.sturdy@gmail.com>
2021-10-06 22:05:33 +0100

    checkpoint while debugging

--- pi-setup.py
+++ pi-setup.py
@@ -55,23 +55,28 @@ def main():
         os.system("pip3 install %s" % package)
 
     # Add the user, with their password disabled but ssh login allowed
-    os.system('adduser --disabled-password --gecos "%s" %s gpio' % (args.user, args.name))
+    try:
+        _ = pwd.getpwnam(args.user)
+    except KeyError:
+        os.system('adduser --disabled-password --gecos "%s" %s gpio' % (args.user, args.name))
     print("on desktop, do this: ssh-copy-id -i ~/.ssh/mykey %s@%s" % (args.user, args.host))
 
     with open("/etc/sudoers", 'a') as sudoers:
         sudoers.write("%s    ALL=(ALL:ALL) ALL\n" % args.user)
 
     drive = "/dev/sda1"
+    mountpoint = "/mnt/hdd0"
     if os.path.exists(drive):
         if not in_fstab(drive):
             system("umount " + drive)
-            if not os.file.exists("/etc/fstab-old"):
+            if not os.path.exists("/etc/fstab-old"):
                 shutil.copy_file("/etc/fstab", "/etc/fstab-old")
             with open("/etc/fstab", 'a') as fstab:
-                fstab.write("%s /mnt/hdd0 ext4 defaults 0 0\n" %s drive)
+                fstab.write("%s %s ext4 defaults 0 0\n" % (drive, mountpoint))
+            os.makedirs(mountpoint, exists_ok=True)
             system("mount " + drive)
-        if os.path.isdir("/mnt/hdd0"):
-            for filename in glob.glob("/mnt/hdd0/*"):
+        if os.path.isdir(mountpoint):
+            for filename in glob.glob(mountpoint+"/*"):
                 os.symlink(os.path.join(home_directory, os.path.basename(filename)), filename)
 
     # Do the rest as the new user
